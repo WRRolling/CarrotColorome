c438198a53ac1e17bed8f2bf151929767e22ca58
WRRolling <63259851+WRRolling@users.noreply.github.com>
2024-01-05 12:55:11 -0600

    Delete Analysis_16_fst_Dist directory
    Updated Analyses

--- Analysis_16_fst_Dist/AverageFst.R
+++
@@ -1,8 +0,0 @@
-library('tidyverse')
-dat <- read_table(file="CA.fst.weir.fst", col_names=T)
-mean(dat$WEIR_AND_COCKERHAM_FST)
-# 0.1458919
-
-dat <- read_table(file="WI.fst.weir.fst", col_names=T)
-mean(dat$WEIR_AND_COCKERHAM_FST)
-# 0.14
